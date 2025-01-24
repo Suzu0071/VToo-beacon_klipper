035db1eb14ba0c242ec7226890786a6cf19a3bf4
Lasse Dalegaard <dalegaard@gmail.com>
2025-01-24 16:16:25 +0100

    Fix

--- beacon.py
+++ beacon.py
@@ -1472,7 +1472,7 @@ class BeaconProbe:
         self.printer.send_event("homing:home_rails_begin", homing_state, [])
         self.mcu_contact_probe.activate_gcode.run_gcode_from_command()
         try:
-            self.compat_toolhead_set_position_homing_z(toolhead, force_pos)
+            self.compat_toolhead_set_position_homing_z(self.toolhead, force_pos)
             skip_next = True
             retries = 0
             while len(stop_samples) < sample_count:
